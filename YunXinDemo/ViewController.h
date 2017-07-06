2836c42acc102fc4b0f91933e4b1713c0ff5cdd1
HCMac <hcmac@HCMacdeMac-mini.local>
2017-07-06 10:21:26 +0800

    Initial Commit

---
+++ YunXinDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YunXinDemo
+//
+//  Created by HCMac on 2017/7/6.
+//  Copyright © 2017年 demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YunXinDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  YunXinDemo
+//
+//  Created by HCMac on 2017/7/6.
+//  Copyright © 2017年 demo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
